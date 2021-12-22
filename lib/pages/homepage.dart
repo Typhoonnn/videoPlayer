3c7fb443603ba7176bb1b7554789f0387f02d00d
Ying Zheng <ying.zheng@Yings-MacBook-Pro.local>
2021-12-22 19:31:53 +0800

    提交homepage布局

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,12 +6,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("主页")),
-        body: CatogeryList(),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+          title: Padding(
+          padding: EdgeInsets.fromLTRB(24, 30, 24, 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('Prime ', style: TextStyle(color: Color.fromRGBO(0, 185, 231, 1))),
+              Text('Video', style: TextStyle(color: Colors.white))
+            ],
+          ),
+        )),
+        body: MainContent(),
+        backgroundColor: Colors.black
       );
   }
 }
 
+class MainContent extends StatefulWidget {
+  MainContent({Key? key}) : super(key: key);
+
+  @override
+  _MainContentState createState() => _MainContentState();
+}
+
+class _MainContentState extends State<MainContent> {
+  // fake data
+  List imageList = [1, 2, 3, 4, 5];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CatogeryList(),
+        ImageList(list: imageList),
+        Padding(
+          padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+          child: Text("最新发布", style: TextStyle(fontSize: 19, color: Color.fromRGBO(0, 185, 231, 1)), textAlign: TextAlign.left),
+        )
+      ]
+    );
+  }
+}
 class CatogeryList extends StatefulWidget {
   CatogeryList({Key? key}) : super(key: key);
 
@@ -20,10 +57,12 @@ class CatogeryList extends StatefulWidget {
 }
 
 class _CatogeryListState extends State<CatogeryList> {
+  int _activeIndex = 0;
   List categoryList = [];
   
   getData () {
-    List arr = [
+    // todo: getdata from backend
+    List list = [
       {
         "type_id": 1,
         "type_name": "电影"
@@ -49,22 +88,9 @@ class _CatogeryListState extends State<CatogeryList> {
         "type_name": "体育"
       },
     ];
-
-    List list = arr.map((value) {
-      return Container(
-            width: 110,
-            alignment: Alignment.center,
-            child: Text("${value["type_name"]}", style: TextStyle(color: Colors.white)),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Color.fromRGBO(0, 185, 231, 1)
-            )
-          );
-    }).toList();
     
     setState(() {
       categoryList = list;
-      print("category list${categoryList}");
     });
   }
   @override
@@ -72,51 +98,78 @@ class _CatogeryListState extends State<CatogeryList> {
     super.initState();
     getData();
   }
+  
   Widget build(BuildContext context) {
     return Container(
       height: 40,
-      // child: ListView(
-      //   scrollDirection: Axis.horizontal,
-      //   children: <Widget>[
-      //     Container(
-      //       width: 110,
-      //       alignment: Alignment.center,
-      //       child: Text("文本1", style: TextStyle(color: Colors.white)),
-      //       decoration: BoxDecoration(
-      //         borderRadius: BorderRadius.circular(20),
-      //         color: Color.fromRGBO(0, 185, 231, 1)
-      //       )
-      //     ),
-      //     Container(
-      //       width: 180,
-      //       color: Colors.blue,
-      //       child: Text("文本2"),
-      //     ),
-      //     Container(
-      //       width: 180,
-      //       color: Colors.blue,
-      //       child: Text("文本3"),
-      //     ),
-      //     Container(
-      //       width: 180,
-      //       color: Colors.blue,
-      //       child: Text("文本4"),
-      //     ),
-      //     Container(
-      //       width: 180,
-      //       color: Colors.blue,
-      //       child: Text("文本5"),
-      //     )
-      //   ],
-      // ),
+      padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: categoryList.length,
         itemBuilder: (BuildContext context, int index) {
-          return categoryList[index];
+          return GestureDetector(
+            child: Container(
+              width: 110,
+              alignment: Alignment.center,
+              child: Text(
+                  "${categoryList[index]["type_name"]}",
+                  style: TextStyle(color: _activeIndex == index ? Colors.black : Colors.white)
+                ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: _activeIndex == index ? Color.fromRGBO(0, 185, 231, 1) : Color.fromRGBO(0, 0, 0, 0)
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                _activeIndex = index;
+              });
+            },
+          );
+          
         },
       ),
     );
   }
 }
 
+
+class ImageList extends StatelessWidget {
+  const ImageList({
+    Key? key,
+    required this.list
+  }) : super(key: key);
+
+  final List list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 172,
+      margin: EdgeInsets.fromLTRB(0, 40, 0, 24),
+      padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            child: Container(
+              width: 306,
+              padding: EdgeInsets.fromLTRB(0, 0, 24, 0),
+              child: Container(
+                  decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromRGBO(0, 185, 231, 1)
+                )
+              ),
+            ),
+            onTap: () {
+              print("tap image item");
+            },
+          );
+          
+        },
+      ),
+    );
+  }
+}

bf56b1a6764f7ae9d2d42ffdfe3d43d043718ec1
Ying Zheng <ying.zheng@Yings-MacBook-Pro.local>
2021-12-23 18:03:38 +0800

    提交home page切图代码

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+//           title: Padding(
+//           padding: EdgeInsets.fromLTRB(24, 30, 24, 20),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               Text('Prime ', style: TextStyle(color: Color.fromRGBO(0, 185, 231, 1))),
+//               Text('Video', style: TextStyle(color: Colors.white))
+//             ],
+//           ),
+//         )),
+//         body: MainContent(),
+//         backgroundColor: Colors.black
+//       );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,14 +42,34 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(24, 30, 24, 20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+            children: const [
               Text('Prime ', style: TextStyle(color: Color.fromRGBO(0, 185, 231, 1))),
               Text('Video', style: TextStyle(color: Colors.white))
             ],
           ),
         )),
         body: MainContent(),
-        backgroundColor: Colors.black
+        backgroundColor: Colors.black,
+        bottomNavigationBar: Container(
+            height: 100,
+            padding: const EdgeInsets.only(top: 10.0),
+            child: BottomNavigationBar(
+            backgroundColor: const Color.fromRGBO(37, 46, 57, 1),
+            selectedItemColor: const Color.fromRGBO(0, 185, 231, 1),
+            unselectedItemColor: const Color.fromRGBO(90, 103, 119, 1),
+            currentIndex: _currentPageIndex,
+            onTap: (int index) {
+              setState(() {
+                _currentPageIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home, size: 30,), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.search, size: 30,), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.settings, size: 30), label: "")
+            ]
+          ),
+        )
       );
   }
 }
@@ -34,6 +84,8 @@ class MainContent extends StatefulWidget {
 class _MainContentState extends State<MainContent> {
   // fake data
   List imageList = [1, 2, 3, 4, 5];
+  List videoList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +96,8 @@ class _MainContentState extends State<MainContent> {
         Padding(
           padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
           child: Text("最新发布", style: TextStyle(fontSize: 19, color: Color.fromRGBO(0, 185, 231, 1)), textAlign: TextAlign.left),
-        )
+        ),
+        Expanded(child: VideoList(list: videoList))
       ]
     );
   }
@@ -166,9 +219,51 @@ class ImageList extends StatelessWidget {
             onTap: () {
               print("tap image item");
             },
-          );
-          
+          ); 
         },
+      ),
+    );
+  }
+}
+
+class VideoList extends StatelessWidget {
+  const VideoList({
+    Key? key,
+    required this.list
+  }) : super(key: key);
+
+  final List list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      child: GridView.builder (
+      itemCount: list.length,
+      shrinkWrap: true, //Container跟随GridView内容变化高度
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.6,
+        mainAxisSpacing: 22.0,
+        crossAxisSpacing: 24.0,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            child: Container(
+              width: 160,
+              height: 202,
+              child: Container(
+                  decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromRGBO(22, 22, 22, 1)
+                )
+              ),
+            ),
+            onTap: () {
+              print("tap video item");
+            },
+          );
+        }
       ),
     );
   }
